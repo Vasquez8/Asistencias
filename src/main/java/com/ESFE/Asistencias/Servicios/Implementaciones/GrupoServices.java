e7f321053ca9bbef94422e4a1e6ca8f6351e3196
Vasquez8 <dv113409@gmail.com>
2024-07-31 13:25:45 -0600

    Nuevos cambios

---
+++ src/main/java/com/ESFE/Asistencias/Servicios/Implementaciones/GrupoServices.java
@@ -0,0 +1,44 @@
+package com.ESFE.Asistencias.Servicios.Implementaciones;
+
+import com.ESFE.Asistencias.Entidades.Grupo;
+import com.ESFE.Asistencias.Repositorio.IGrupoRepository;
+import com.ESFE.Asistencias.Servicios.Interfaces.IGrupoServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GrupoServices implements IGrupoServices {
+
+    @Autowired
+    private IGrupoRepository grupoRepository;
+
+    @Override
+    public Page<Grupo> BuscarTodosPaginados(Pageable pageable) {
+        return grupoRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Grupo> ObtenerTodos() {
+        return grupoRepository.findAll();
+    }
+
+    @Override
+    public Optional<Grupo> BuscarporId(Integer id) {
+        return grupoRepository.findById(id);
+    }
+
+    @Override
+    public Grupo CreaOeditar(Grupo grupo) {
+        return grupoRepository.save(grupo);
+    }
+
+    @Override
+    public void EliminarPorId(Integer id) {
+        grupoRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/ESFE/Asistencias/Servicios/Interfaces/IGrupoServices.java
@@ -0,0 +1,17 @@
+package com.ESFE.Asistencias.Servicios.Interfaces;
+
+import com.ESFE.Asistencias.Entidades.Grupo;
+import org.springframework.data.domain.Page; //Agregar import de spring
+import org.springframework.data.domain.Pageable; //Agregar import de spring
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IGrupoServices {
+
+    Page<Grupo> BuscarTodosPaginados(Pageable pageable);
+    List<Grupo> ObtenerTodos();
+    Optional<Grupo> BuscarporId(Integer id);
+    Grupo CreaOeditar(Grupo grupo);
+    void EliminarPorId(Integer id);
+}
